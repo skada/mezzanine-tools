2191bbc902b2ed9f4b18817c52a0f36abb3d62c8
Jakub Skaryd <skaryd@gmail.com>
2016-09-18 22:29:45 +0200

    still trying to add templates

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     name='mezzanine_tools',
     version=mezzanine_tools.__version_str__,
     packages=find_packages(exclude=('sandbox',)),
+    include_package_data=True,
     url='',
     license='BSD',
     author='Jakub Skaryd',
