1ffda03f55aae7aabc817b62d52e37c5660ad3f7
Jakub Škaryd <skaryd@gmail.com>
2024-11-27 19:53:47 +0100

    update

--- sandbox/sandbox/urls.py
+++ sandbox/sandbox/urls.py
@@ -1,7 +1,5 @@
-from __future__ import unicode_literals
-
-from django.conf.urls import include, url
-from django.conf.urls.i18n import i18n_patterns
+from django.urls import include, path
+from django.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.views.i18n import set_language
 
@@ -18,12 +16,12 @@ admin.autodiscover()
 urlpatterns = i18n_patterns(
     # Change the admin prefix here to use an alternate URL for the
     # admin interface, which would be marginally more secure.
-    url("^admin/", include(admin.site.urls)),
+    path("admin/", include(admin.site.urls)),
 )
 
 if settings.USE_MODELTRANSLATION:
     urlpatterns += [
-        url('^i18n/$', set_language, name='set_language'),
+        path('i18n/', set_language, name='set_language'),
     ]
 
 urlpatterns += [
@@ -37,7 +35,7 @@ urlpatterns += [
     # one homepage pattern, so if you use a different one, comment this
     # one out.
 
-    url("^$", direct_to_template, {"template": "index.html"}, name="home"),
+    path("/$", direct_to_template, {"template": "index.html"}, name="home"),
 
     # HOMEPAGE AS AN EDITABLE PAGE IN THE PAGE TREE
     # ---------------------------------------------
@@ -77,7 +75,7 @@ urlpatterns += [
     # ``mezzanine.urls``, go right ahead and take the parts you want
     # from it, and use them directly below instead of using
     # ``mezzanine.urls``.
-    url("^", include("mezzanine.urls")),
+    path("", include("mezzanine.urls")),
 
     # MOUNTING MEZZANINE UNDER A PREFIX
     # ---------------------------------
